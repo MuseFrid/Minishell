2c8abf922f488d8cb965c0a910f1c7573e07e9c3
Alexis Abda <alexis.abda@hotmail.com>
2023-04-29 20:39:56 +0200

    delete builtins function

--- srcs/env/getenv.c
+++ srcs/env/getenv.c
@@ -6,30 +6,11 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 12:51:54 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/19 02:04:05 by aabda            ###   ########.fr       */
+/*   Updated: 2023/04/29 20:39:25 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-/*
-
-ft_getenv:
-...
-
-ft_ensure :
-Check if current exist, if current doesn't exist = exit
-loop on current to find if the ft_strncmp find the variable to ensure
-	if the variable is the first one on the list :
-		change the head of the list on the next one,
-		put the prev on NULL and free current.
-
-	if the variable is not the first element of the list :
-		the variable next of the previous element = next element
-		if a next element exist,
-		the prev variable of the next element = previous
-		and free current.
-*/
 
 static void	ft_add_value(t_data *data, t_env *new, char **envp, int i)
 {
@@ -69,34 +50,5 @@ void	ft_getenv(t_data *data, char **envp)
 		if (!new)
 			exit(EXIT_FAILURE);
 		ft_add_value(data, new, envp, i);
-	}
-}
-
-void	ft_ensure(t_data *data, char *value)
-{
-	t_env	*current;
-
-	current = data->env;
-	if (!current)
-		exit(EXIT_FAILURE);
-	while (current)
-	{
-		if (ft_strncmp(value, current->value, ft_strlen(value)) == 0)
-		{
-			if (current == data->env)
-			{
-				data->env = current->next;
-				current->next->prev = NULL;
-			}
-			else
-			{
-				current->prev->next = current->next;
-				if (current->next)
-					current->next->prev = current->prev;
-			}
-			free(current);
-			break ;
-		}
-		current = current->next;
 	}
 }
