ee1fd3db01b6f573340b859925fba6469ef13488
Alexis Abda <alexis.abda@hotmail.com>
2023-04-29 20:43:50 +0200

    ft_unset take only one parameter

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 22:23:01 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/28 23:52:29 by aabda            ###   ########.fr       */
+/*   Updated: 2023/04/29 20:42:03 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	ft_exit(t_data *data);
 int	ft_env(t_data *data);
-int	ft_unset(t_data *data, char *value);
+int	ft_unset(t_data *data);
 int	ft_pwd(t_data *data);
 
 #endif
