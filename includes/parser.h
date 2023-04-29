15f11be94b46f6cff3a34730903bfdc492ef9eb4
Alexis Abda <alexis.abda@hotmail.com>
2023-04-29 20:34:01 +0200

    norme 42 for parser.h

--- includes/parser.h
+++ includes/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gduchesn <gduchesn@student.s19.be>         +#+  +:+       +#+        */
+/*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 14:30:38 by gduchesn          #+#    #+#             */
-/*   Updated: 2023/04/27 18:48:53 by gduchesn         ###   ########.fr       */
+/*   Updated: 2023/04/28 23:12:34 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 typedef struct s_simple_cmds
 {
-	char **str;
-	//int (*builtin)(t_tools *, struct s_simple_cmds *);
-	int num_redirections;
-	char *hd_file_name;
-	t_arg *redirections;
-	t_arg *test_red;
-	struct s_simple_cmds *next;
-	struct s_simple_cmds *prev;
+	char					**str;
+	// int						(*builtin)(t_tools *, struct s_simple_cmds *);
+	int						num_redirections;
+	char					*hd_file_name;
+	t_arg					*redirections;
+	t_arg					*test_red;
+	struct s_simple_cmds	*next;
+	struct s_simple_cmds	*prev;
 }	t_simple_cmds;
 
 void			parser(t_arg *arg, t_data data);
