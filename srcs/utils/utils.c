a680e23aade36dc6c8159da2bdd8a912c4c1574b
Alexis Abda <alexis.abda@hotmail.com>
2023-04-29 20:34:50 +0200

    add utils.c

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/19 01:18:05 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/28 22:53:45 by aabda            ###   ########.fr       */
+/*   Created: 2023/04/28 22:53:06 by aabda             #+#    #+#             */
+/*   Updated: 2023/04/28 23:32:52 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_H
-# define UTILS_H
+#include "../../includes/minishell.h"
 
-size_t	ft_strlen(char *str);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	ft_init_struct(t_data *data, char **envp);
-
-#endif
+void	ft_init_struct(t_data *data, char **envp)
+{
+	data->err_return_val = 0;
+	data->env = NULL;
+	ft_getenv(data, envp);
+}
