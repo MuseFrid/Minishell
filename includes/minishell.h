132599ec0c3441c7f8d14acb69f016af3124aabb
Alexis Abda <alexis.abda@hotmail.com>
2023-04-29 20:39:01 +0200

    add include and delete ft_strlen function

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,13 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:11:21 by gduchesn          #+#    #+#             */
-/*   Updated: 2023/04/28 21:12:23 by aabda            ###   ########.fr       */
+/*   Updated: 2023/04/28 22:52:07 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
 # include <stdio.h>
 # include <readline/readline.h>
 # include <unistd.h>
@@ -21,16 +22,18 @@
 # include <dirent.h>
 # include <string.h>
 # include <signal.h>
+# include "core.h"
 # include "color.h"
 # include "lexer.h"
-# include "core.h"
+# include "env.h"
+# include "builtins.h"
 # include "parser.h"
+# include "utils.h"
 
 # define SUCCESS 0
 # define FAIL 1
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_strlen(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *str);
 //char	*parse(char **envp, const char *cmd);
