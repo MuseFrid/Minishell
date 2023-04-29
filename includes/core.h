6063ab7c3dd48f0e5058d2158e1334901a84f80c
Alexis Abda <alexis.abda@hotmail.com>
2023-04-29 20:32:57 +0200

    put structure env in env.h

--- includes/core.h
+++ includes/core.h
@@ -6,29 +6,18 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 01:49:58 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/28 21:12:07 by aabda            ###   ########.fr       */
+/*   Updated: 2023/04/28 22:59:00 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CORE_H
 # define CORE_H
 
-typedef struct s_env
-{
-	int				index;
-	char			*value;
-	struct s_env	*next;
-	struct s_env	*prev;
-}	t_env;
-
 typedef struct s_data
 {
+	int				err_return_val;
+	char			*line;
 	struct s_env	*env;
 }	t_data;
-
-void	ft_init_struct(t_data *data, char **envp);
-void	ft_getenv(t_data *data, char **envp);
-void	ft_unset(t_data *data, char *value);
-void	ft_init_struct(t_data *data, char **envp);
 
 #endif
--- includes/env.h
+++ includes/env.h
@@ -6,14 +6,21 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 01:43:03 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/19 02:09:56 by aabda            ###   ########.fr       */
+/*   Updated: 2023/04/28 23:52:11 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
+typedef struct s_env
+{
+	int				index;
+	char			*value;
+	struct s_env	*next;
+	struct s_env	*prev;
+}	t_env;
+
 void	ft_getenv(t_data *data, char **envp);
-void	ft_ensure(t_data *data, char *value);
 
 #endif
