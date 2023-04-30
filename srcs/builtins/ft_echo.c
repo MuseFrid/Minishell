cdf7a1b450e37bd04e082e64a295a07bbc90451e
Alexis Abda <alexis.abda@hotmail.com>
2023-04-30 20:28:55 +0200

    add builtin echo.c

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.h                                         :+:      :+:    :+:   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/28 22:23:01 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/30 20:28:29 by aabda            ###   ########.fr       */
+/*   Created: 2023/04/30 05:37:54 by aabda             #+#    #+#             */
+/*   Updated: 2023/04/30 06:00:10 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTINS_H
-# define BUILTINS_H
+#include "../../includes/minishell.h"
 
-int	ft_exit(t_data *data);
-int	ft_env(t_data *data);
-int	ft_unset(t_data *data);
-int	ft_pwd(t_data *data);
-int	ft_export(t_data *data);
-int	ft_echo(t_data *data);
+int	ft_echo(t_data *data)
+{
+	(void)data;
+	char	**value;
+	int		i;
 
-#endif
+	// value = 					need to put the value of the struct
+	i = 0;
+	while (value[i])
+		i++;
+	if (i == 3 && ft_strncmp(value[1], "-n", 2) == 0)
+		printf("%s", value[2]);
+	else if (i == 2)
+		printf("%s\n", value[1]);
+	else
+		printf("\n");
+	return (0);
+}
