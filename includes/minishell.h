b0740e2e2b713ede1173a8b0cff1a7f688841913
Alexis Abda <alexis.abda@hotmail.com>
2023-05-05 05:09:15 +0200

    delete useless prototype

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:11:21 by gduchesn          #+#    #+#             */
-/*   Updated: 2023/04/28 22:52:07 by aabda            ###   ########.fr       */
+/*   Updated: 2023/05/05 05:07:22 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,5 @@
 
 # define SUCCESS 0
 # define FAIL 1
-
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *str);
-//char	*parse(char **envp, const char *cmd);
-int		ga_error(char *error_msg);
 
 #endif
