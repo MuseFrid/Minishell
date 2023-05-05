9951008c7aab9a1603a46a58b1de85e689e06c74
Alexis Abda <alexis.abda@hotmail.com>
2023-05-05 05:10:16 +0200

    add ft_strdup

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 01:18:05 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/28 22:53:45 by aabda            ###   ########.fr       */
+/*   Updated: 2023/05/05 05:06:44 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 size_t	ft_strlen(char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_init_struct(t_data *data, char **envp);
+char	*ft_strdup(char *s);
 
 #endif
--- srcs/utils/libft_utils.c
+++ srcs/utils/libft_utils.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 01:16:35 by aabda             #+#    #+#             */
-/*   Updated: 2023/04/19 01:58:16 by aabda            ###   ########.fr       */
+/*   Updated: 2023/05/05 05:06:31 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,22 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		i++;
 	}
 	return (0);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*str;
+	int		i;
+
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
