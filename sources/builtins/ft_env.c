972011dbc86eac00498fb30a25ab4c02f1eb41a0
Alexis Abda <alexis.abda@hotmail.com>
2023-05-27 23:49:11 +0200

    sorted env if cmd is only "export"

--- sources/builtins/ft_env.c
+++ sources/builtins/ft_env.c
@@ -6,23 +6,113 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 21:39:36 by aabda             #+#    #+#             */
-/*   Updated: 2023/05/19 03:01:11 by aabda            ###   ########.fr       */
+/*   Updated: 2023/05/27 23:47:37 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	ft_count_var_env(t_data *data)
+{
+	t_env	*current;
+	int		nbr_var_env;
+
+	current = data->env;
+	nbr_var_env = 0;
+	while (current)
+	{
+		current = current->next;
+		++nbr_var_env;
+	}
+	return (nbr_var_env);
+}
+
+static char	**ft_fill_array(t_data *data, char **tab, int size)
+{
+	t_env	*current;
+	int		i;
+
+	current = data->env;
+	i = 0;
+	while (i < size)
+	{
+		tab[i] = current->key;
+		++i;
+		current = current->next;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
+
+static void	ft_bubble_sort(char **tab, int size)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = -1;
+	while (++i < size - 1)
+	{
+		j = -1;
+		while (++j < size - i - 1)
+		{
+			if (ft_strncmp(tab[j], tab[j + 1], ft_strlen(tab[j])) > 0)
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+static void	ft_env_sorted(t_data *data)
+{
+	t_env	*current;
+	char	**env_sorted;
+	int		nbr_var_env;
+	int		i;
+
+	nbr_var_env = ft_count_var_env(data);
+	env_sorted = malloc(sizeof(char *) * (nbr_var_env + 1));
+	if (!env_sorted)
+		exit(EXIT_FAILURE);		//	need to put error function
+	env_sorted = ft_fill_array(data, env_sorted, nbr_var_env);
+	ft_bubble_sort(env_sorted, nbr_var_env);
+	i = 0;
+	while (env_sorted[i])
+	{
+		current = data->env;
+		while (ft_strcmp_strict(current->key, env_sorted[i]) != 0)
+			current = current->next;
+		if (ft_strcmp_strict(current->key, env_sorted[i]) == 0)
+			printf("declare -x %s=\"%s\"\n", current->key, current->value);
+		i++;
+	}
+	free(env_sorted);
+	env_sorted = NULL;
+}
+
 int	ft_env(t_data *data)
 {
 	t_env	*current;
+	int		argc;
 
 	current = data->env;
 	if (!current)
 		return (1);
-	while (current)
+	argc = 0;
+	while (data->cmds->str[argc])
+		++argc;
+	if (argc == 1 && ft_strcmp_strict(data->cmds->str[0], "export") == 0)
+		ft_env_sorted(data);
+	else
 	{
-		printf("%s=%s\n", current->key, current->value);
-		current = current->next;
+		while (current)
+		{
+			printf("%s=%s\n", current->key, current->value);
+			current = current->next;
+		}
 	}
 	return (0);
 }
--- sources/utils/builtins.c
+++ sources/utils/builtins.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 16:34:11 by aabda             #+#    #+#             */
-/*   Updated: 2023/05/12 20:59:03 by aabda            ###   ########.fr       */
+/*   Updated: 2023/05/27 22:07:11 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ int	ft_check_builtins(t_data *data)
 {
 	int	(*builtin[8])(t_data *);
 	int	res;
+	int	argc;
 
 	builtin[0] = &ft_cd;
 	builtin[1] = &ft_echo;
@@ -50,8 +51,13 @@ int	ft_check_builtins(t_data *data)
 	builtin[6] = &ft_unset;
 	builtin[7] = NULL;
 	res = ft_builtins_tab(data->cmds->str[0]);
+	argc = 0;
+	while (data->cmds->str[argc])
+		++argc;
 	if (res < 0)
 		data->cmds->builtin = NULL;
+	else if (argc == 1 && ft_strcmp_strict(data->cmds->str[0], "export") == 0)
+		data->cmds->builtin = builtin[2];
 	else
 		data->cmds->builtin = builtin[res];
 	return (0);
